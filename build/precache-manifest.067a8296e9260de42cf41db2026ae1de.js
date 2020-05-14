ff5022c66f8d7851dcb7468de317e9f0dd93906e
tmchavez96 <tmchavez96@gmail.com>
2020-05-14 07:06:55 -0700

    adding build to save time

---
+++ build/precache-manifest.067a8296e9260de42cf41db2026ae1de.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd9b04aac668f141078a3192b09a67d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74004a57ea64b8c0454d",
+    "url": "/static/css/main.35132e41.chunk.css"
+  },
+  {
+    "revision": "159e1757ba9661e09377",
+    "url": "/static/js/2.95aeb035.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.95aeb035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74004a57ea64b8c0454d",
+    "url": "/static/js/main.4fc414d0.chunk.js"
+  },
+  {
+    "revision": "a3f0e6363adb81a926f9",
+    "url": "/static/js/runtime-main.c3ecdd11.js"
+  },
+  {
+    "revision": "af8a497a39bc15ca697bad2ed282c41d",
+    "url": "/static/media/BCHblack.af8a497a.png"
+  },
+  {
+    "revision": "3a9d6dbda1ad35d3fd6dd425f4cb8171",
+    "url": "/static/media/bellLogoTrans.3a9d6dbd.png"
+  },
+  {
+    "revision": "1ae665c07adb5188a4db7caacf34bd27",
+    "url": "/static/media/coffeeRoundTable.1ae665c0.jpg"
+  },
+  {
+    "revision": "5a69c256f0a91fe373bd2f13135cafbe",
+    "url": "/static/media/dot-grid.5a69c256.png"
+  },
+  {
+    "revision": "339975fca222473125f4e5e3e6326738",
+    "url": "/static/media/hotDrink.339975fc.jpg"
+  },
+  {
+    "revision": "4dbb9a0bbb9eaae3b584d54deb2b417c",
+    "url": "/static/media/muchCoffee.4dbb9a0b.jpg"
+  },
+  {
+    "revision": "d86ca0dccf5a6c667a4dc95b9bee74e2",
+    "url": "/static/media/openDuringCovid.d86ca0dc.png"
+  },
+  {
+    "revision": "484bfe813fa83b838da404bc9606d231",
+    "url": "/static/media/steamingCoffeeMug.484bfe81.jpg"
+  },
+  {
+    "revision": "0ae5c365ef0d0ae7ead27d509c284496",
+    "url": "/static/media/steamingMug.0ae5c365.jpg"
+  }
+]);
